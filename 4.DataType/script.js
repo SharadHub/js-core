030111a293ba5a62146576324fc0a5d3620f5277
Sharad Bista <bishalbista737@gmail.com>
2026-04-06 23:04:37 +0545

    feat: add Data Types module - primitives and reference types

---
+++ 4.DataType/script.js
@@ -0,0 +1,24 @@
+// Numbers
+
+let age = 23;
+let phoneNumber = 9866123946;
+
+console.log(`I am ${age} years old and My phone number is ${phoneNumber}.`);
+
+// Strings
+
+let firstName = "Bishal";
+let lastName = "Bista";
+fullName = firstName + " " + lastName;
+
+console.log(`My name is ${fullName}`);
+
+// Booleans
+
+let absenteeNumber = 12;
+let isAbsent = true;
+let isMale = false;
+
+console.log(`Number of absent students today : ${absenteeNumber} `);
+console.log(`Ram was absent today. True or False? ${isAbsent}`);
+console.log(`there are 10 male absentees today. True or False? ${isMale}`)
